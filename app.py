eeb2f9605c2fe1be4055677531c3bdc22411c938
gunasreekimidi7 <kimidigunasree7@gmail.com>
2026-02-10 16:29:17 +0530

    added new database

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template, request, jsonify
-import mysql.connector
+import os
+import psycopg2
 
 app = Flask(__name__)
 
-# Connect MySQL
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="WJ28@krhps",
-    database="giftsdb"
-)
+# Connect PostgreSQL using Render Environment Variable
+DATABASE_URL = os.environ.get("postgresql://lucky_gits_user:23PaLy5szgW5I2AaiTLrJS6HmByfy3Hk@dpg-d65ggol6ubrc7394esug-a/lucky_gits")
 
+db = psycopg2.connect(DATABASE_URL)
 cursor = db.cursor()
 
 @app.route('/')
 def home():
-
     return render_template('index.html')
 
 @app.route('/get_gift', methods=['POST'])
 def get_gift():
     number = request.json['number']
 
-    cursor.execute("SELECT gift_name FROM gifts WHERE id=%s", (number,))
+    cursor.execute(
+        "SELECT gift_name FROM gifts WHERE id = %s",
+        (number,)
+    )
     result = cursor.fetchone()
 
     if result:
@@ -31,4 +30,4 @@ def get_gift():
         return jsonify({"gift": "No Gift Found"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
